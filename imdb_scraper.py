3b476e6fd4aa3f5b6a262fcfced2e2d31b9df0b1
Kunal Ramchurn <kunalsingh.ramchurn@postgrad.manchester.ac.uk>
2020-06-14 11:41:29 +0100

    updated code to cover the full 1000 list with updated output

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -12,11 +12,10 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 
-url = "https://www.imdb.com/search/title/?groups=top_1000&ref_=adv_prv"
-headers = {"Accept-Language": "en-US, en;q=0.5"}
-results = requests.get(url, headers=headers)
+from time import sleep
+from random import randint
 
-soup = BeautifulSoup(results.text, "html.parser")
+headers = {"Accept-Language": "en-US, en;q=0.5"}
 
 titles = []
 years = []
@@ -26,40 +25,49 @@ metascores = []
 votes = []
 us_gross = []
 
-movie_div = soup.find_all('div', class_='lister-item mode-advanced')
+pages = np.arange(1,1001,50)
 
-for container in movie_div:
+for page in pages:
 
-        #name
-        name = container.h3.a.text
-        titles.append(name)
+    results = requests.get("https://www.imdb.com/search/title/?groups=top_1000&start=" + str(page) + "&ref_=adv_nxt", headers=headers)
 
-        #year
-        year = container.h3.find('span', class_='lister-item-year').text
-        years.append(year)
+    soup = BeautifulSoup(results.text, "html.parser")
+    movie_div = soup.find_all('div', class_='lister-item mode-advanced')
 
-        # runtime
-        runtime = container.p.find('span', class_='runtime').text if container.p.find('span', class_='runtime').text else '-'
-        time.append(runtime)
+    sleep(randint(2,10))
 
-        #IMDb rating
-        imdb = float(container.strong.text)
-        imdb_ratings.append(imdb)
+    for container in movie_div:
 
-        #metascore
-        m_score = container.find('span', class_='metascore').text if container.find('span', class_='metascore') else '-'
-        metascores.append(m_score)
+            #name
+            name = container.h3.a.text
+            titles.append(name)
 
-        #there are two NV containers, they hold both the votes and the grosses
-        nv = container.find_all('span', attrs={'name': 'nv'})
+            #year
+            year = container.h3.find('span', class_='lister-item-year').text
+            years.append(year)
 
-        #filter nv for votes
-        vote = nv[0].text
-        votes.append(vote)
+            # runtime
+            runtime = container.p.find('span', class_='runtime').text if container.p.find('span', class_='runtime').text else ''
+            time.append(runtime)
 
-        #filter nv for gross earnings
-        grosses = nv[1].text if len(nv) > 1 else '-'
-        us_gross.append(grosses)
+            #IMDb rating
+            imdb = float(container.strong.text)
+            imdb_ratings.append(imdb)
+
+            #metascore
+            m_score = container.find('span', class_='metascore').text if container.find('span', class_='metascore') else ''
+            metascores.append(m_score)
+
+            #there are two NV containers, they hold both the votes and the grosses
+            nv = container.find_all('span', attrs={'name': 'nv'})
+
+            #filter nv for votes
+            vote = nv[0].text
+            votes.append(vote)
+
+            #filter nv for gross earnings
+            grosses = nv[1].text if len(nv) > 1 else '-'
+            us_gross.append(grosses)
 
 #create pandas dataframe
 movies = pd.DataFrame({
@@ -72,11 +80,16 @@ movies = pd.DataFrame({
 'us_grossMillions': us_gross,
 })
 
-#cleaning data
-movies['year'] = movies['year'].str.extract('(\d+)').astype(int)
-movies['timeMin'] = movies['timeMin'].str.extract('(\d+)').astype(int)
-movies['metascore'] = movies['metascore'].astype(int)
 movies['votes'] = movies['votes'].str.replace(',', '').astype(int)
+
+movies.loc[:,'year'] = movies['year'].str[-5:-1].astype(int)
+
+movies['timeMin'] = movies['timeMin'].astype(str)
+movies['timeMin'] = movies['timeMin'].str.extract('(\d+)').astype(int)
+
+movies['metascore'] = movies['metascore'].str.extract('(\d+)')
+movies['metascore'] = pd.to_numeric(movies['metascore'], errors='coerce')
+
 movies['us_grossMillions'] = movies['us_grossMillions'].map(lambda x: x.lstrip('$').rstrip('M'))
 movies['us_grossMillions'] = pd.to_numeric(movies['us_grossMillions'], errors='coerce')
 
